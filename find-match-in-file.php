f60c47cbf2d626da9bad25b1fc065b8d334d9dae
Jayke Peters <jaykepeters@gmail.com>
2019-05-28 09:32:00 -0500

    Update find-match-in-file.php

--- find-match-in-file.php
+++ find-match-in-file.php
@@ -1,15 +1,17 @@
-function findmatch($string, $file) {
-    $matches = array();
-    $handle = @fopen($file, "r");
-    if ($handle)
-    {
-        while (!feof($handle))
+<?php
+    function findmatch($string, $file) {
+        $matches = array();
+        $handle = @fopen($file, "r");
+        if ($handle)
         {
-            $buffer = fgets($handle);
-            if(strpos($buffer, $string) !== FALSE)
-                $matches[] = $buffer;
+            while (!feof($handle))
+            {
+                $buffer = fgets($handle);
+                if(strpos($buffer, $string) !== FALSE)
+                    $matches[] = $buffer;
+            }
+            fclose($handle);
         }
-        fclose($handle);
-    }
-    print_r($matches);
+        print_r($matches);
 }
+?>
