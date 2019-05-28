65964b4f9c3aeebe1f01e7a2d509c004c41ae36e
Jayke Peters <jaykepeters@gmail.com>
2019-05-28 09:31:22 -0500

    Create find-match-in-file.php

---
+++ find-match-in-file.php
@@ -0,0 +1,15 @@
+function findmatch($string, $file) {
+    $matches = array();
+    $handle = @fopen($file, "r");
+    if ($handle)
+    {
+        while (!feof($handle))
+        {
+            $buffer = fgets($handle);
+            if(strpos($buffer, $string) !== FALSE)
+                $matches[] = $buffer;
+        }
+        fclose($handle);
+    }
+    print_r($matches);
+}
